4481aa058da9b6e5c5f61fa258b0957b3d7e6a59
Oulugbek <ulugbekxolmanov419@gmail.com>
2021-12-24 21:08:34 +0500

    task17

--- basic_func15.py
+++ basic_func15.py
@@ -2,7 +2,7 @@
 # Create function arguments a.
 # returns the modulus of a and b.
 def main(a):
-    
+       
 # Example:
 # Input: a = 23 b = 5
 # Output: 3
--- basic_func17.py
+++ basic_func17.py
@@ -1,7 +1,10 @@
 # Create a function called main.
 # Create function arguments a.
 # Assign the value pi to the variable "a" and return.
-
+import math
+def main(a):
+    a = math.pi
+    return a
 # Example:
 # Input: a = pi
 # Output: pi
