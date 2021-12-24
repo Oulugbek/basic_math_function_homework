992765db2d5e06fae2dbd979b5a889ecdc06683a
Oulugbek <ulugbekxolmanov419@gmail.com>
2021-12-24 11:51:42 +0500

    task13

--- basic_func13.py
+++ basic_func13.py
@@ -1,7 +1,8 @@
 # Create a function called main.
 # Create function argument a.
 # Returns the square root of a number.
-
+def main(a):
+    return pow(a,0.5)
 # Example:
 # Input: a = 9
 # Output: 3.0
