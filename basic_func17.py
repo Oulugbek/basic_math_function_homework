bcbe1616bb5648d7410c695a95e2d26f41533843
Oulugbek <ulugbekxolmanov419@gmail.com>
2021-12-24 21:14:24 +0500

    task17

--- basic_func17.py
+++ basic_func17.py
@@ -1,8 +1,9 @@
 # Create a function called main.
 # Create function arguments a.
 # Assign the value pi to the variable "a" and return.
+import math
 def main(a):
-    a = "pi"
+    a = math.pi
     return a
 # Example:
 # Input: a = pi
