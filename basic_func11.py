57355cc1fa539f6e697d60c961e7375891957dff
Oulugbek <ulugbekxolmanov419@gmail.com>
2021-12-24 09:41:43 +0500

    task 1

--- basic_func11.py
+++ basic_func11.py
@@ -1,7 +1,9 @@
 # Create a function called main.
 # Create function argument a.
-# Return the absolute value of a.
-
+# Return the absolute value of 
+import math
+def main(a):
+    return abs(a)
 # Example:
 # Input: a = -8
 # Output: 8
