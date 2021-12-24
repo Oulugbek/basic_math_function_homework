ce1f09756afa5699f70c4281e9abfa0ace509298
Oulugbek <ulugbekxolmanov419@gmail.com>
2021-12-24 20:57:41 +0500

    task16

--- basic_func15.py
+++ basic_func15.py
@@ -1,7 +1,8 @@
 # Create a function called main.
 # Create function arguments a.
 # returns the modulus of a and b.
-
+def main(a):
+    
 # Example:
 # Input: a = 23 b = 5
 # Output: 3
--- basic_func16.py
+++ basic_func16.py
@@ -1,7 +1,9 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Returns the sum of a and b.
-
+def main(a,b):
+    return a+b
+print(main(3,2))
 # Example:
 # Input: a = 3 b = 2
 # Output: 5
