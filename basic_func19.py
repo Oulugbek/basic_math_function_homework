2d518ea07e51099e5e806ec51c932a3b20d5043e
Oulugbek <ulugbekxolmanov419@gmail.com>
2021-12-24 21:21:24 +0500

    task19

--- basic_func19.py
+++ basic_func19.py
@@ -1,7 +1,9 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Return the absolute value of the difference between a and b.
-
+def main(a,b):
+    return round(a-b)
+    
 # Example:
 # Input: a = 4 b = 11
 # Output: 7
